a42d92b11f612789985e5ba570741e99d496e53a
Luana Silva <luanab97silva@gmail.com>
2023-09-03 17:43:52 +0100

    feat: add support for gitlab

--- content.js
+++ content.js
@@ -1,87 +1,12 @@
-const REGEX = /^(?:\/([^\/]+)\/([^\/]+))?\/pulls$/
-
-function get(token, query) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest()
-        request.onreadystatechange = () => {
-            if (request.readyState == 4) {
-                if (request.status != 200) {
-                    reject()
-                } else {
-                    const body = JSON.parse(request.responseText)
-                    if (body.errors?.length > 0) {
-                        reject()
-                    } else {
-                        resolve(body.data)
-                    }
-                }
-            }
-        }
-        request.open('POST', 'https://api.github.com/graphql', true)
-        request.setRequestHeader('Authorization', `Bearer ${token}`)
-        request.send(`{ "query": "{ ${query.replace(/\s+/g, ' ').replace(/"/g, '\\"')} }" }`)
-    })
-}
-
-async function getPullRequestDiffStat(token, org, repo, id) {
-    const query = `repository(owner: "${org}", name: "${repo}") {
-        pullRequest(number: ${id}) {
-            changedFiles
-            additions
-            deletions
-        }
-    }`
-    return (await get(token, query)).repository.pullRequest
-}
-
-function appendSpan(div, style, id, value) {
-    const span = document.createElement('span')
-    span.className = `${style} ml-1`
-    span.id = id
-    span.textContent = value
-    div.append(span)
-}
-
-function updateSpan(div, id, value) {
-    div.querySelector(`[id="${id}"]`).textContent = value
-}
-
-function injectHtml(div, diff) {
-    if (!div.querySelector('[id="stats"]')) {
-        const element = document.createElement('div')
-        element.id = 'stats'
-        appendSpan(element, 'Counter', 'changedFiles', diff.changedFiles)
-        appendSpan(element, 'color-fg-success', 'additions', '+' + diff.additions)
-        appendSpan(element, 'color-fg-danger', 'deletions', '-' + diff.deletions)
-        div.querySelector('[class="opened-by"]').parentNode.append(element)
-    } else {
-        updateSpan(div, 'changedFiles', diff.changedFiles)
-        updateSpan(div, 'additions', '+' + diff.additions)
-        updateSpan(div, 'deletions', '-' + diff.deletions)
-    }
-}
+const DOMAIN_REGEX = /([^\.]+)\.\w+$/
 
 async function run() {
-    try {
-        const { token } = await chrome.storage.sync.get('token')
-        const match = document.location.pathname.match(REGEX)
-        if (!token || !match) {
-            return
-        }
-        const divs = document.body.querySelectorAll('div[id^=issue_]')
-        const promises = []
-        for (const div of divs) {
-            const [, id] = div.id.match(/^issue_(\d+)/)
-            const [, org, repo] =
-                match[0] === '/pulls' ? div.id.match(/^issue_\d+_([^_]+)_([^_]+)$/) : match
-            const promise = getPullRequestDiffStat(token, org, repo, id)
-            promises.concat(promise.then((diff) => injectHtml(div, diff)))
-        }
-        await Promise.all(promises)
-        chrome.runtime.sendMessage({ success: true })
-    } catch (error) {
-        chrome.runtime.sendMessage({ success: false })
-    }
+    const [, source] = document.location.hostname.match(DOMAIN_REGEX)
+    const { inject } = await import(chrome.runtime.getURL(`sources/${source}.js`))
+    const { options } = await chrome.storage.sync.get('options')
+    await inject(options ?? {}, document.location.pathname)
+        .then(() => chrome.runtime.sendMessage({ success: true, source }))
+        .catch(() => chrome.runtime.sendMessage({ success: false, source }))
 }
 
 chrome.runtime.onMessage.addListener(run)
--- options.js
+++ options.js
@@ -1,15 +1,23 @@
 const MESSAGE_TIMEOUT = 2 * 1000
 
-const restoreOptions = async () => {
-    const { token } = await chrome.storage.sync.get('token')
-    document.getElementById('token').value = token
+async function restoreOptions() {
+    const { options } = await chrome.storage.sync.get('options')
+    document.getElementById('github_token').value = options?.github?.token || ''
+    document.getElementById('gitlab_token').value = options?.gitlab?.token || ''
 }
 
-const saveOptions = async () => {
-    const token = document.getElementById('token').value
-    await chrome.storage.sync.set({ token })
+async function saveOptions() {
+    const options = {
+        github: {
+            token: document.getElementById('github_token').value,
+        },
+        gitlab: {
+            token: document.getElementById('gitlab_token').value,
+        },
+    }
+    await chrome.storage.sync.set({ options })
     const status = document.getElementById('status')
-    status.textContent = 'Token saved successfully'
+    status.textContent = 'Options saved successfully'
     setTimeout(() => (status.textContent = ''), MESSAGE_TIMEOUT)
 }
 
